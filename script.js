ae580f12ac0c90f5ae65d2e8217f34006918b146
Daniel Oliver <danieloliver19871@icloud.com>
2026-07-07 18:28:03 +0100

    Add more single round logic + bug fixes

--- script.js
+++ script.js
@@ -19,41 +19,38 @@ function getComputerChoice(Rock, Paper, Scissors) {
         resultString = "Scissors";
     }
 
-    // print the value
-    console.log(resultString);
+    // return the new value
+    return resultString;
 
 }
-
-// invoke the function
-getComputerChoice();
 
             // *** HUMAN CHOICE ***
 
 // declare function for human choice
-function getHumanChoice(Rock, Paper, Scissors) {
+function getHumanChoice() {
 
 // prompt human to make a choice
 
    let humanChoice = prompt("Make your choice");
+   const choiceLower = humanChoice?.toLowerCase(); 
 
 // return alert based on choice
 
-    if (humanChoice === "Rock") {
+    if (choiceLower === "rock") {
         alert("You chose Rock!");
     }
 
-    else if (humanChoice === "Paper") {
+    else if (choiceLower === "paper") {
         alert("You chose Paper!");
     }
 
-    else if (humanChoice === "Scissors") {
+    else if (choiceLower === "scissors") {
         alert("You chose Scissors!");
     }
 
+// return the users input
+    return humanChoice;
 }
-
-// invoke the function 
-    getHumanChoice();
 
             // *** KEEP TRACK OF THE SCORE ***
 
@@ -65,9 +62,14 @@ function getHumanChoice(Rock, Paper, Scissors) {
 // declare function for a single round
 function playRound(humanChoice, computerChoice) {
 
+// crash protection
+    if (!humanChoice || !computerChoice) {
+        return "Error: A choice was missing.";
+    }
+
 // make case insensitive
     const human = humanChoice.toLowerCase();
-    const computer = computerChoice.toLowercase();
+    const computer = computerChoice.toLowerCase();
 
 // if humanChoice Rock - computerChoice Paper - computer wins 
     if (human === "rock" && computer === "paper") {
@@ -75,29 +77,40 @@ function playRound(humanChoice, computerChoice) {
         return "Computer wins! Paper beats Rock.";
     }
 // if humanChoice Rock - computerChoice Scissors - human wins
-// if humanChoice Rock - computerChoice Rock - null
-
-// if humanChoice Paper - computerChoice Paper - null
-// if humanChoice Paper - computerChoice Rock - human wins 
-// if humanChoice Paper - computerChoice Scissors - computer wins
-
-// if humanChoice Scissors - computerChoice Scissors - null
-// if humanChoice Scissors - computerChoice Paper - human wins 
-// if humanChoice Scissors - computerChoice Rock - computer wins
-
-
-  
+    else if (human === "rock" && computer === "scissors") {
+        humanScore ++;
+        return "You win! Rock beats Scissors."
 }
 
+// if humanChoice Paper - computerChoice Rock - human wins 
+    else if (human === "paper" && computer === "rock") {
+        humanScore ++;
+        return "You win! Paper beats Rock."
+}
+// if humanChoice Paper - computerChoice Scissors - computer wins
+    else if (human === "paper" && computer === "scissors") {
+        computerScore ++;
+        return "Computer wins! Scissors beats Paper."
+}
+
+// if humanChoice Scissors - computerChoice Paper - human wins 
+    else if (human === "scissors" && computer === "paper") {
+        humanScore ++;
+        return "You win! Scissors beats Paper."
+}
+// if humanChoice Scissors - computerChoice Rock - computer wins
+    else if (human === "scissors" && computer === "rock") {
+        computerScore ++;
+        return "Computer wins! Rock beats Scissors."
+}
+// if humanChoice & computerChoice the same
+    else {
+        return "It's a tie! Try again.";
+    }
+}
+    
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
-
-
-
-
-
-
-
-
+console.log(playRound(humanSelection, computerSelection));
+console.log(`Score -> You: ${humanScore} | Computer: ${computerScore}`);
